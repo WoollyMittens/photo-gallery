bc9b692a94a83d3e853382cbfd47992ededdfe65
Maurice van Creij <maurice.vancreij@webqem.com>
2014-07-08 14:15:21 +1000

    Reduced reliance on typography.

--- inc/js/gallery.js
+++
@@ -1,8 +0,0 @@
-/*
-	Combined JavaScripts
-
-	@codekit-append "../src/useful.gallery.js"
-	@codekit-append "../../useful-polyfills/src/useful.polyfills.js"
-	@codekit-append "../../useful-requests/src/useful.requests.js"
-	@codekit-append "../../useful-transitions/src/useful.transitions.js"
-*/
