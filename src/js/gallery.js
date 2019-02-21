c09b84245ff278df9c29646cb85413c68cd499b6
Maurice van Creij <maurice.vancreij@webqem.com>
2019-02-22 07:50:08 +1100

    Update require support.

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -34,6 +34,5 @@ var Gallery = function (config) {
 };
 
 // return as a require.js module
-if (typeof module !== 'undefined') {
-	exports = module.exports = Gallery;
-}
+if (typeof define != 'undefined') define(['gallery'], function () { return Gallery });
+if (typeof module != 'undefined') module.exports = Gallery;
